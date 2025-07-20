b52ad5039d6b90ad69229352851d1318497514c4
kirillmuhanko <79082614+kirillmuhanko@users.noreply.github.com>
2025-07-20 18:36:04 +0300

    Remove `[Authorize]` attribute from `PingController` and simplify `Get` response payload.

--- src/Company.Identity.Api/Ping/Controllers/PingController.cs
+++ src/Company.Identity.Api/Ping/Controllers/PingController.cs
@@ -1,16 +1,14 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Identity.Api.Ping.Controllers;
 
 [ApiController]
 [Route("api/ping")]
-[Authorize]
 public class PingController : ControllerBase
 {
     [HttpGet]
     public IActionResult Get()
     {
-        return Ok(new { message = "Pong", user = User.Identity?.Name });
+        return Ok("Pong");
     }
 }
